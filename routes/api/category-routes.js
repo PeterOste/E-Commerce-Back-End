20b1ee3dad36bbb5afe3f9a41b94fbdb14ef833c
unknown <peter.ostertag99@gmail.com>
2023-08-08 14:16:15 -0500

    Updated file paths and sourced seeds

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../../fantastic-umbrella/Develop/models');
+const { Category, Product } = require('../models');
 
 // The `/api/categories` endpoint
 
