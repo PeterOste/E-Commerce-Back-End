e314f4560a7518503ced2c1ecdd88f962081ab58
unknown <peter.ostertag99@gmail.com>
2023-08-08 16:59:14 -0500

    Synced Sequelize to server.js and created column in Category.js

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,18 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// Created column
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
