1a5ce8cdb5702a918cbe4b8ae16f39a7e2c8ff15
unknown <peter.ostertag99@gmail.com>
2023-08-10 15:47:50 -0500

    Get request for single category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,9 +15,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// Get one category by 'id' and its associated products
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: Product, // Includes associated Products
+    });
+    if (!category) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
