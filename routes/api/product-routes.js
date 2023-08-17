db2efe9eabba0ea71b322615cedc35c1c4c65497
unknown <peter.ostertag99@gmail.com>
2023-08-17 17:26:25 -0500

    Added get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,25 @@ const { Product, Category, Tag, ProductTag } = require('../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    // find all products and include its associated Category and Tag data
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['id', 'category_name'],
+        },
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name'],
+        },
+      ],
+    });
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
