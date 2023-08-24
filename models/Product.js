6cc7d78160c841f06408d2e4f99dac75c83132d5
unknown <peter.ostertag99@gmail.com>
2023-08-24 14:15:44 -0500

    Fixed seeding

--- models/Product.js
+++ models/Product.js
@@ -37,7 +37,7 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Category', // Must be capital?
+        model: 'category',
         key: 'id',
       },
     },
